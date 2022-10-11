2eff6410f4e940e803beb8dcebbd5f3c11bde7ad
Ronnie <rbeggs10@gmail.com>
2022-10-11 11:18:23 -0700

    Converted static home page into a set of react components. Attempted to organize file structure.

---
+++ my-page/src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <span><a href="">Home</a></span>
+                <span><a href="https://github.com/ronniebeggs">Work</a></span>
+                <span><a href="contact.html">Contact</a></span>
+            </nav>
+        </header>
+    );
+};
+
+export default Header
+        
